81a09a295d5ad8c1d500e49ab07592568041fd3f
Daniel-Hohler <danielhohler8667@gmail.com>
2023-10-12 21:51:23 -0400

    changed function but still no work

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from matplotlib.animation import FuncAnimation
 
 randomList = random.sample(range(100), 100)
 lst = list(range(0,100))
-yValue = randomList
-def quick_sort(sequence):
-    length = len(sequence)
-    if length <= 1:
-        return sequence
-    else:
-        pivot = sequence.pop()
-    
-    items_greater = []
-    items_lower = []
 
-    for item in sequence:
-        if item > pivot:
-            items_greater.append(item)
-        else:
-            items_lower.append(item)
-    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+def partition(array, low, high):
+
+  # choose the rightmost element as pivot
+  pivot = array[high]
+
+  # pointer for greater element
+  i = low - 1
+
+  # traverse through all elements
+  # compare each element with pivot
+  for j in range(low, high):
+    if array[j] <= pivot:
+      # if element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+
+      # swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+
+  # swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+  # return the position from where partition is done
+  return i + 1
+
+
+
+# function to perform quicksort
+def quickSort(array, low, high):
+  if low < high:
+
+    # find pivot element such that
+    # element smaller than pivot are on the left
+    # element greater than pivot are on the right
+    pi = partition(array, low, high)
+
+    # recursive call on the left of pivot
+    quickSort(array, low, pi - 1)
+
+    # recursive call on the right of pivot
+    quickSort(array, pi + 1, high)
+
+
+
+
+size = len(randomList)
+
+
+
 fig = plt.figure(figsize= (10, 5))
 plt.bar(randomList, lst, color='maroon', width = .4)
-plt.show()
+nim = FuncAnimation(fig, func=partition, fargs=(randomList, 0, size-1), interval = 100)
+plt.show() 
+
+quickSort(randomList, 0, size - 1)
+
+print(randomList)
